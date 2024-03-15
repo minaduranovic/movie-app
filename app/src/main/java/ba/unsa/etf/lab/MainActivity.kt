5c3a05fb8cf6e19c1319d6941830f25058bf2c3c
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-03-15 14:33:03 +0100

    zad3

--- app/src/main/java/ba/unsa/etf/lab/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/lab/MainActivity.kt
@@ -1,6 +1,9 @@
 package ba.unsa.etf.lab
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,13 +12,16 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val button = findViewById<Button>(R.id.button1);
+        button.setOnClickListener {
+            showMessage()
         }
     }
+    private fun showMessage() {
+        val editText = findViewById<EditText>(R.id.editText1)
+        val textView = findViewById<TextView>(R.id.text1)
+        val message = editText.text.toString()
+        textView.text = message
+    }
 }
-//
