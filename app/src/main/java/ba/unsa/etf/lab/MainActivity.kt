8406dd245506b35f44a97d7608456de8a136abb5
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-03-19 14:07:35 +0100

    zad1

--- app/src/main/java/ba/unsa/etf/lab/MainActivity.kt
+++ app/src/main/java/ba/unsa/etf/lab/MainActivity.kt
@@ -1,8 +1,10 @@
 package ba.unsa.etf.lab
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,18 +12,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var listView: ListView
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+    private val  listaVrijednosti = arrayListOf<String>()
+    private lateinit var adapter : ArrayAdapter<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button1);
+        button = findViewById(R.id.button1);
+        editText = findViewById(R.id.editText1)
+        listView = findViewById(R.id.listView1)
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaVrijednosti)
+        listView.adapter=adapter
         button.setOnClickListener {
-            showMessage()
+            addToList()
         }
     }
-    private fun showMessage() {
-        val editText = findViewById<EditText>(R.id.editText1)
-        val textView = findViewById<TextView>(R.id.text1)
-        val message = editText.text.toString()
-        textView.text = message
+    private fun addToList() {
+        listaVrijednosti.add(0,editText.text.toString())
+        adapter.notifyDataSetChanged();
+        editText.setText("");
     }
 }
