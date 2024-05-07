c6b4d62ca518303208aa726f8439395da1086c7d
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-05-07 12:55:18 +0200

    vjezba 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,10 +39,8 @@ dependencies {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
@@ -58,4 +56,9 @@ dependencies {
 //    testImplementation("org.hamcrest:hamcrest:x.y")
     androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
     testImplementation("org.hamcrest:hamcrest:2.2")
+
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.github.bumptech.glide:glide:5.0.0-rc01")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1-Beta")
 }
--- app/src/main/java/ba/unsa/etf/lab/MovieListAdapter.kt
+++ app/src/main/java/ba/unsa/etf/lab/MovieListAdapter.kt
@@ -7,38 +7,47 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MovieListAdapter(
     private var movies: List<Movie>,
     private val onItemClicked: (movie:Movie) -> Unit
 ) : RecyclerView.Adapter<MovieListAdapter.MovieViewHolder>() {
+
+    private val posterPath = "https://image.tmdb.org/t/p/w342"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val view = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.item_movie, parent, false)
         return MovieViewHolder(view)
     }
-
     override fun getItemCount(): Int = movies.size
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        holder.movieTitle.text = movies[position].title;
-        val genreMatch: String = movies[position].genre
+        holder.movieTitle.text = movies[position].title
+        val genreMatch: String? = movies[position].genre
+        //Pronalazimo id drawable elementa na osnovu naziva zanra
         val context: Context = holder.movieImage.context
-        var id: Int = context.resources
-            .getIdentifier(genreMatch, "drawable", context.packageName)
-        if (id == 0) id = context.resources
-            .getIdentifier("picture1", "drawable", context.packageName)
+        var id: Int = 0;
+        if (genreMatch!==null)
+            id = context.getResources()
+                .getIdentifier(genreMatch, "drawable", context.getPackageName())
+        if (id===0) id=context.getResources()
+            .getIdentifier("picture1", "drawable", context.getPackageName())
         holder.movieImage.setImageResource(id)
-
         holder.itemView.setOnClickListener{ onItemClicked(movies[position]) }
-
+        Glide.with(context)
+            .load(posterPath + movies[position].posterPath)
+            .centerCrop()
+            .placeholder(R.drawable.picture1)
+            .error(id)
+            .fallback(id)
+            .into(holder.movieImage);
+        holder.itemView.setOnClickListener{ onItemClicked(movies[position]) }
     }
-
     fun updateMovies(movies: List<Movie>) {
         this.movies = movies
         notifyDataSetChanged()
     }
-
     inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val movieImage: ImageView = itemView.findViewById(R.id.movieImage)
         val movieTitle: TextView = itemView.findViewById(R.id.movieTitle)
